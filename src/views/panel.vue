cb3e6a1806ba4a39ecdf36a91c992a40870512e3
Miguel Ocampo <miguelocampoc@gmail.com>
2022-02-19 19:13:36 -0500

    ok

--- src/views/panel.vue
+++ src/views/panel.vue
@@ -229,11 +229,11 @@
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">{{ruta}}</li>
+          <ol  class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a   @click="back()" class="opacity-5 text-dark">Atras</a ></li>
+            <li  class="breadcrumb-item text-sm text-dark active" aria-current="page">{{this.$router.currentRoute._rawValue.name}}</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">{{ruta}}</h6>
+          <h6 class="font-weight-bolder mb-0">{{this.$router.currentRoute._rawValue.name}}</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -463,6 +463,7 @@ export default {
         return ""
 
       },
+     
     },
     mounted(){
       let route= this.$router.currentRoute._rawValue.name;
@@ -471,7 +472,9 @@ export default {
      console.log(this.ruta);
     },
     methods:{
-    
+     back(){
+         this.$router.go(-1);
+      },
     logout(){
         localStorage.removeItem('auth');
         location.reload();
